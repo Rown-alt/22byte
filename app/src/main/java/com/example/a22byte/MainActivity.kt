b29d94c7fc16f5ae8f84f60ac957af8534f7f874
Rown-alt <79279060+Rown-alt@users.noreply.github.com>
2023-06-27 23:24:16 +0300

    Update MainActivity

--- app/src/main/java/com/example/a22byte/MainActivity.kt
+++ app/src/main/java/com/example/a22byte/MainActivity.kt
@@ -2,12 +2,27 @@ package com.example.a22byte
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.a22byte.viewModels.MainActivityViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel  by viewModel<MainActivityViewModel>()
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.supportActionBar?.hide()
 
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
 
+        viewModel.addBooks() // если в бд уже есть книги, то ничего не делает
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
