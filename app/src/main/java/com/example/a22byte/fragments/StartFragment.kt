7cf8ae7052af6549cdf1437c7886bf814f6215c8
Rown-alt <79279060+Rown-alt@users.noreply.github.com>
2023-06-27 23:23:48 +0300

    StartFragmentInit

---
+++ app/src/main/java/com/example/a22byte/fragments/StartFragment.kt
@@ -0,0 +1,33 @@
+package com.example.a22byte.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.a22byte.databinding.StartFragmentBinding
+
+class StartFragment: Fragment() {
+    private var _binding: StartFragmentBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = StartFragmentBinding.inflate(inflater, container, false)
+        binding.playButton.setOnClickListener {
+            val action = StartFragmentDirections.actionStartFragmentToQuestionFragment()
+            it.findNavController().navigate(action)
+        }
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
